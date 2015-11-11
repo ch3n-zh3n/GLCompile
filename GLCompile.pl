8189dd27e1e3f943c05717dceec3258a519e7297
Jack <chen.dzen@gmail.com>
2015-11-11 07:54:41 +0200

    Create GLCompile.pl

---
+++ GLCompile.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use Term::ANSIColor;
+
+print color('MAGENTA');
+
+$usage = "GLCompile.pl /home/<user>/GLPROJECT/<first argument> \n or \ndefault:  /home/<user>/GLPROJECT/CppApplication_1/main.cpp \n";
+
+if($ARGV[0] eq '-?')
+ {print $usage;}
+  else{
+  if($ARGV[0])
+{ system "g++ -w -lGL -lglut -lGLU /home/".getpwuid( $< )."/GLPROJECT/".$ARGV[0]."  -o GLFILE";}
+else
+{ system "g++ -w -lGL -lglut -lGLU /home/".getpwuid( $< )."/GLPROJECT/CppApplication_1/main.cpp -o GLFILE";}
+ }
+
+print color('RESET');
